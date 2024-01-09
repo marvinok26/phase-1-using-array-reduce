8440d80e26143ca6ad48fea6af926981a9c21e7c
Marvin <okongomarvin971@gmail.com>
2024-01-09 09:00:27 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
-
-
-
-// Assuming totalBatteries is a variable in the global scope
-
-
-
-
 // Calculate the total number of batteries
 let totalBatteries = batteryBatches.reduce((acc, current) => acc + current, 0);
